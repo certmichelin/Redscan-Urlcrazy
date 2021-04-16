7082aa234f51d95f015eed83793932036b2ceffb
razieldrake <vayssierpro@gmail.com>
2021-04-16 16:07:44 +0200

    Init and first release

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# redscan-urlcrazy
+## REDSCAN-URLCRAZY
+
+| Attribute     | Value                                        |
+| ------------- | -------------------------------------------- |
+| Subscribe to  |           MASTERDOMAINS                      |
+| Send to       |                NONE                          |
+| Tools used    |                urlcrazy                      |
+| Configuration | /conf/log4j2.xml : Log4j2 configuration file |
+
+### Objective
+
+Monitor registered exotic domains name which can be used for Phishing , Url Hijacking, typo-squatting and affiliates.
+
+
+### Description
+
+Brute force domains names and requests for them.
+
+### How to develop
+
+```
+docker run -d -p 5672:5672 -p 15672:15672 --name redscan-rabbit-dev rabbitmq:3-management
+docker run -d -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" --name redscan-elasticsearch-dev docker.elastic.co/elasticsearch/elasticsearch:7.10.1
+
+# If cache is required for the plugin
+docker run -d -p 8080:8080 --name redscan-cache-dev docker.pkg.github.com/certmichelin/redscan-cache/redscan-cache:latest
+```
---
+++ src/main/java/com/michelin/cert/redscan/RabbitMqConfig.java
@@ -0,0 +1,43 @@
+/**
+ * Michelin CERT 2020.
+ */
+
+package com.michelin.cert.redscan;
+
+import com.michelin.cert.redscan.utils.queueing.RabbitMqBaseConfig;
+
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Declarables;
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Configure Rabbit MQ messages.
+ *
+ * @author Maxime ESCOURBIAC
+ * @author Sylvain VAISSIER
+ * @author Maxence SCHMITT
+ */
+@Configuration
+public class RabbitMqConfig extends RabbitMqBaseConfig {
+
+  //TODO: Configure interaction.
+  
+  ///**
+  // * QUEUE_DOMAINS.
+  // */
+  public static final String QUEUE_MASTERDOMAINS = "com.michelin.cert.shodan.masterdomains";
+  /**
+  * Queue configuration method.
+  *
+  * @return Declarables.
+  */
+    @Bean
+    public Declarables fanoutBindings() {
+      Queue queue = new Queue(QUEUE_MASTERDOMAINS, false);
+      FanoutExchange fanoutExchange = new FanoutExchange(FANOUT_MASTERDOMAINS_EXCHANGE_NAME, false, false);
+      return new Declarables(queue, fanoutExchange, BindingBuilder.bind(queue).to(fanoutExchange));
+    }
+}
